91109b5b26c86d9b9e15f9cf5c2f8e7b4ee0f2ab
coder31337 <coder31337@coder31337>
2024-04-03 14:05:49 +0000

    Add code of p4

---
+++ Python/src/p4_largest_num.py
@@ -0,0 +1,9 @@
+first = int(input('Enter first number: '))
+second = int(input('Enter second number: '))
+
+if first > second:
+    print('First number is larger')
+elif second > first:
+    print('Second number is larger')
+else:
+    print('Both number are equal')
